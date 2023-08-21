84434c4b8298c047be250ad0bcdccd33561d697c
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2023-08-21 11:38:25 +0000

    ...

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,20 @@
 const mynums = [1,2,3,4,5,6,7,8,9,10]
 
-const numbers = mynums.filter((numbers) => numbers > 4 )
 
-console.log(numbers);
+/* const numbers = mynums.filter((numbers) => numbers > 4 )
+   console.log(numbers); */
+
+
+// or filter can be use it as
+
+
+/*const numbers = mynums.filter((numbers) =>{
+        return numbers > 4 })
+        
+        console.log(numbers);
+ */
+
+
+//and if we don't use these two method then we use foreach method 
+
+ console.log(numbers);
