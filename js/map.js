f62527902336e6a0a8f8db4d34a6b4876fd09d4d
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2023-08-22 17:05:11 +0000

    ...

---
+++ js/map.js
@@ -0,0 +1,7 @@
+// map is used in array to change the value for example too  add, subtract whatever you want to do with array..     
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const result = numbers.map( (num) => num + 5)
+    console.log(result)
