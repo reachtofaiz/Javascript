b839f3bab64a5f64df9551346898452cc14d14d9
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2023-08-23 11:50:02 +0000

    ...

--- js/filter, map and reduce/reduce.js
+++ js/filter, map and reduce/reduce.js
@@ -4,8 +4,9 @@ const number = [1, 2, 3]
 
 const arr = number.reduce( (init, num) => {
 
+        console.log(`initial_value: ${init} and secondary_value: ${num}`)  //  for inspect we use backtick `
         return init + num
 
-}, 0)
+}, 0) // value is initialized here
 
 console.log(arr)
