3884848cad2d86d239fe2bb700087ad5ac227390
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2023-08-23 12:12:10 +0000

    ...

--- js/filter, map and reduce/reduce.js
+++ js/filter, map and reduce/reduce.js
@@ -1,5 +1,6 @@
 // reduse is used for adding and initial value is given by user
 
+/*
 const number = [1, 2, 3]
 
 const arr = number.reduce( (init, num) => {
@@ -10,3 +11,34 @@ const arr = number.reduce( (init, num) => {
 }, 0) // value is initialized here
 
 console.log(arr)
+*/
+
+// example
+
+const shoppingCart = [
+        {
+            itemName: "js course",
+            price: 2999
+        },
+        {
+            itemName: "py course",
+            price: 999
+        },
+        {
+            itemName: "mobile dev course",
+            price: 5999
+        },
+        {
+            itemName: "data science course",
+            price: 12999
+        },
+    ]
+
+
+    const result = shoppingCart.reduce( (initial,item) =>{
+
+            console.log(`initial_value = ${initial} and secondary_value = ${item.price}`)
+            return initial + item.price
+
+    }, 0)
+    console.log("Final price = %d",result)
