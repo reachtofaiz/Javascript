ac21b18e74ffcf6fedf15a61e20ea96b24012cad
Mohammad Faiz <121545406+reachtofaiz@users.noreply.github.com>
2023-09-09 18:51:47 +0000

    Need Updates

--- js/Projects/Guess the number/index.js
+++ js/Projects/Guess the number/index.js
@@ -1,4 +1,4 @@
-const randomNumber = parseInt(Math.random() * 100 + 1) 
+const randomNumber = parseInt(Math.random() * 100 + 1)
 
 const submit = document.querySelector('#submit')
 const input = document.querySelector('#input')
@@ -15,34 +15,50 @@ let gamestart = 1
 let playGame = true;
 
 if (playGame) {
-    submit.addEventListener('click',function(e){
+    submit.addEventListener('click', function (e) {
         e.preventDefault()
         const guess = parseInt(input.value);
         console.log(guess);
         validation(guess)
-    })    
+    })
 }
 
-function validation(guess){
+function validation(guess) {
+    if (isNaN(guess)) {
+        alert("Please Enter valid number")
+    } else if (guess > 100) {
+        alert("Enter less than 100")
+    } else if (guess < 1) {
+        alert("Enter more than one")
+    } else {
+        previousguess.push(guess);
+        if (gamestart === 11) {
+            displayguess(guess)
+            displaymessage(`Game Over ... random number is ${randomNumber}`);
+            endGame();
+        } else {
+            displayguess();
+            checkguess();
+        }
+    }
+}
+
+function checkguess(guess) {
 
 }
 
-function checkguess(guess){
+function displayguess(guess) {
 
 }
 
-function displayguess(guess){
+function displaymessage(message) {
 
 }
 
-function displaymessage(message){
+function endGame() {
 
 }
 
-function endGame(){
-
-}
-
-function newGame(){
+function newGame() {
 
 }
